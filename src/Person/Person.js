a7f2443fc00022e530d163ddc234eedc8d47d9f4
Quan Nguyen <quanguyen250@gmail.com>
2020-02-15 16:02:47 -0500

    React udemy course

---
+++ src/Person/Person.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+// import './Person.css';
+
+  const StyledDiv = styled.div `
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+
+    '@media (min-width: 500px)' : {
+      width: 450px;
+    }
+  `;
+
+const person = props => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  };
+
+  return (
+    <StyledDiv>
+      {/* <div className="Person" style={style}> */}
+        <p>My name is {props.name} and I am {props.age}!</p>
+        <p>{props.children}</p>
+        <input className="input" type="text" onChange={props.changed} value={props.name}/>
+        <button onClick={props.click}>Remove this person</button>
+      {/* </div> */}
+    </StyledDiv>
+  );
+}
+
+export default (person);
