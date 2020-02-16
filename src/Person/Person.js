d8633ca68829993595e968f11e46dec36f3d3007
Quan Nguyen <quanguyen250@gmail.com>
2020-02-15 22:17:41 -0500

    css module (alternate css cutomization)

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,37 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-// import './Person.css';
-
-  const StyledDiv = styled.div `
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    '@media (min-width: 500px)' : {
-      width: 450px;
-    }
-  `;
+import classes from './Person.module.css';
 
 const person = props => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  };
-
   return (
-    <StyledDiv>
-      {/* <div className="Person" style={style}> */}
-        <p>My name is {props.name} and I am {props.age}!</p>
-        <p>{props.children}</p>
-        <input className="input" type="text" onChange={props.changed} value={props.name}/>
-        <button onClick={props.click}>Remove this person</button>
-      {/* </div> */}
-    </StyledDiv>
+     <div className={classes.Person}>
+      <p onClick={props.click}>My name is {props.name} and I am {props.age}!</p>
+      <p>{props.children}</p>
+      <input className="input" type="text" onChange={props.changed} value={props.name}/>
+     </div>
   );
 }
 
-export default (person);
+export default person;
