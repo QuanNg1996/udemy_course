3b96f337f0aca056b9e52d72744ea9c4b4f5c6ff
Quan Nguyen <quanguyen250@gmail.com>
2020-02-16 19:41:03 -0500

    Cleaner react code

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,7 +6,7 @@ const person = props => {
      <div className={classes.Person}>
       <p onClick={props.click}>My name is {props.name} and I am {props.age}!</p>
       <p>{props.children}</p>
-      <input className="input" type="text" onChange={props.changed} value={props.name}/>
+      <input type="text" onChange={props.changed} value={props.name}/>
      </div>
   );
 }
