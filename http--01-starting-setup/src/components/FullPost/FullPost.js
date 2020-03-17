fdadecbc938915a9fb5da0d16417ab20c95ca73b
Quan Nguyen <quanguyen250@gmail.com>
2020-03-17 15:24:36 -0400

    catch error

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -12,13 +12,21 @@ class FullPost extends Component {
     if (this.props.id) {
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
         axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-        .then((response) => {
-          this.setState({ loadedPost: response.data })
+          .then((response) => {
+            this.setState({ loadedPost: response.data })
           // console.log(response);
         });
       }
     }
   }
+
+  deletePostHandler = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .then(response => {
+        console.log(response);
+      });
+  }
+
   render () {
       let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
@@ -32,7 +40,7 @@ class FullPost extends Component {
               <h1>{this.state.loadedPost.title}</h1>
               <p>{this.state.loadedPost.body}</p>
               <div className="Edit">
-                  <button className="Delete">Delete</button>
+                  <button onClick={this.deletePostHandler} className="Delete">Delete</button>
               </div>
           </div>
         );
--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null
+    selectedPostId: null,
+    error: false
   }
 
   componentDidMount () {
@@ -24,6 +25,10 @@ class Blog extends Component {
         });
         this.setState({ posts: updatedPosts});
         // console.log(response);
+      })
+      .catch(error => {
+        // console.log(error);
+        this.setState({ error: true});
       });
   }
 
@@ -32,7 +37,9 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
+      let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+      if (!this.state.error) {
+        posts = this.state.posts.map(post => {
           return <Post
               key={post.id}
               title={post.title}
@@ -40,7 +47,7 @@ class Blog extends Component {
               clicked={() => this.postSelectedHandler(post.id)}
             />;
         });
-
+      }
         return (
             <div>
                 <section className="Posts">
