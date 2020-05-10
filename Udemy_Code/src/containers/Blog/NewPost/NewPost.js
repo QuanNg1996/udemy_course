5bbdbd3a633c9b00be4d7dafe785104337fbb5a7
QuanNg1996 <quanguyen250@gmail.com>
2020-05-10 00:22:52 -0400

    auth

--- Udemy_Code/src/containers/Blog/NewPost/NewPost.js
+++ Udemy_Code/src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './NewPost.css';
+import { Redirect } from 'react-router-dom';
 
 class NewPost extends Component {
   state = {
     title: '',
     content: '',
-    author: 'Max'
+    author: 'Max',
+    submitted: false
+  }
+
+  componentDidMount () {
+    // If unauth => this.props.history.replace('/posts');
+    console.log(this.props);
   }
 
   postDataHandler = () => {
@@ -15,15 +22,23 @@ class NewPost extends Component {
       body: this.state.content,
       author: this.state.author
     };
-    axios.post('/posts', data)
-      .then(response => {
-          console.log(response);
-      });
+
+  axios.post('/posts', data)
+    .then(response => {
+      console.log(response);
+      this.props.history.replace('/posts');
+      // this.setState({ submitted: true });
+    });
   }
 
   render () {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
