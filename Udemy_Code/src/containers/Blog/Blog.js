6bad3998e36f632b520342c9da569b79f96a81e2
QuanNg1996 <quanguyen250@gmail.com>
2020-05-11 04:37:09 -0400

    commits

--- Udemy_Code/src/containers/Blog/Blog.js
+++ Udemy_Code/src/containers/Blog/Blog.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
+
   render () {
     return (
       <div className="Blog">
@@ -33,9 +39,10 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/new-post" render={() => <h1>New-Post</h1>} /> */}
         <Switch>
-          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1>Not found</h1>}/>
+          {/* <Redirect from="/" to="/posts" /> */}
           {/* <Route path="/" component={Posts} /> */}
         </Switch>
       </div>
